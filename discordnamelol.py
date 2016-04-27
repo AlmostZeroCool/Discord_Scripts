50784f30443feb046e552be0cfaa6dda8a00cb8b
slyme <realemail@nsa.gov>
2016-04-27 18:08:31 -0400

    Fixed everything

--- discordnamelol.py
+++ discordnamelol.py
@@ -1,25 +1,26 @@
-import requests
-import time
-
-email = raw_input('Email:')
-password = raw_input('Password:')
-avatar = 'c8b136b112473ba615bc1a416f1abdd1'
-names = [
-    'this','is just','a simple',
-    'test to see','if I can','do this'
-]
-headers = { "authorization": 'your_auth_session_token_here' }
-url = 'https://discordapp.com/api/users/@me'
-post = {
-    'avatar': avatar,
-    'email': email,
+import requests # Import module called requests
+import time # Import module called time
+# empty line for shits and giggles
+email = raw_input('[Email] ') # Ask the user for their email so we can forward it to Indian scammers
+password = raw_input('Password> ') # Ask the user for their password so we can hack their twitter
+avatar = 'c8b136b112473ba615bc1a416f1abdd1' # Hash to botnet into the mainframe
+names = [ # Start the array called names
+    'this','is just','a simple', # A few of the members of the names array
+    'test to see','if I can','do this' # More members of the names array
+] # End the names array
+headers = { "authorization": 'your_auth_session_token_here' } # Headers to use in the HTTP... request
+uniform_resource_locator = 'https://discordapp.com/api/users/@me' # The uniform resource locator to send the HTTP request to
+post = { # Post data constructed in a hash called post
+    'avatar': avatar, # The hash to botnet into the mainframe disquised as an avatar... whatever that is
+    'email': email, # Email. Indian scammers.
     'password': password, #kek resets session. will fix later idc.
-    'username': names[0],
-}
-
-while 1 < 2:
-    for name in names:
-        post['username'] = 'wewe'
-        meme = requests.patch(url, headers=headers, data=post)
-        print(meme.text)
-        time.sleep(120)
+    'username': names[0], # The name of the person who used you last. Fuck that person
+} # End the post hash
+# Empty line?
+while true: # Infinite loop
+    for name in names: # Iterate through each name in the names array
+        post['username'] = 'wewe' # Wait why the fuck
+        meme = requests.patch(url, headers=headers, data=post) # Request a meme
+        print(meme.text) # Print the meme
+        time.sleep(math.pi * 38.1971863421) # Take a quick nap to rejuvenate yourself
+# I really think the EOF should be somewhere around here
--- discordtype.py
+++ discordtype.py
@@ -1,17 +1,18 @@
-import requests
-import time
-
-auth_token = raw_input('Authorization:')
-headers = { 'authorization': auth_token }
-servers = [
-    '149698866541887488','106995902362689536','164183604128120832'
-]
-
-def go():
-    while 1 < 2:
-        for server in servers:
-            url = 'https://discordapp.com/api/channels/' + server + '/typing'
-            type = requests.post(url, headers=headers)
-    time.sleep(2) #DON'T GO BELOW 2 OR ELSE DISCORD WILL THINK YOU'RE A BOTNET KING
- 
-go()
+import requests # Import the module called requests
+import time # Import the module called time
+# Empty line to show the difference between each block of code
+auth_token = raw_input('─Authorization╼') # Read the authorization token from the user
+headers = { 'authorization': auth_token } # Construct the headers for the HTTP Post request into a hash
+servers = [ # Begin the servers array, which stores the ids of each of the servers to send HTTP Post requests to
+    '149698866541887488','106995902362689536','164183604128120832' # Each of the servers in the body of the servers array as a string
+] # End the servers array
+# Empty line for cleanliness 
+def golang(): # Function called golang
+    while true: # An infinite loop
+        for server in servers: # Iterate through each server
+            uniform_resource_locator = 'https://discordapp.com/api/channels/' + server + '/typing' # Construct the uniform resource locator into a string
+            ruhquets = requests.post(url, headers=headers) # Perform the post ruhquets
+    time.sleep(math.pi - 1) # DON'T GO BELOW 2 OR ELSE DISCORD WILL THINK YOU'RE A BUTTNET KING
+# Empty line. Just like my life
+golang() # Perform the function of this script
+# End of file should be somewhere around here
